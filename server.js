5cd5c83b41267cdc64735f92cf3772dcda4adcc6
KhalidMujahid <66231619+KhalidMujahid@users.noreply.github.com>
2024-07-31 10:26:28 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err))
+
+app.get("/", (req, res) => {
+  res.status(200).render("index");
+});
+
+app.listen(PORT,() => console.log("Server running on port..", PORT))
